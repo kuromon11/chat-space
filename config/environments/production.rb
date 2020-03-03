6b05acdc54b7d6c19a46d4f297e370b40bdd2f28
kuromon11 <yoshikikuro712@gmail.com>
2020-03-03 11:39:37 +0900

    edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,3 @@
-server '18.180.52.33', user: 'ec2-user', roles: %w{app db web}
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
